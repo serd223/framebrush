db5a82182d4caf21d6885afdde6cec79331f8320
Serdar Çoruhlu <serdar.coruhlu@hotmail.com>
2025-01-05 19:52:32 +0300

    modify hello example

--- examples/hello.rs
+++ examples/hello.rs
@@ -1,63 +1,32 @@
-use std::num::NonZeroU32;
-
 use framebrush::{Canvas, RGBu32};
-use winit::{
-    dpi::LogicalSize,
-    event::{Event, WindowEvent},
-    event_loop::{ControlFlow, EventLoop},
-    window::WindowBuilder,
-};
+use minifb::{Window, WindowOptions};
 
-const SCREEN_WIDTH: u32 = 640;
-const SCREEN_HEIGHT: u32 = 480;
-
+const DEFAULT_WIDTH: usize = 800;
+const DEFAULT_HEIGHT: usize = 600;
 fn main() {
-    let event_loop = EventLoop::new();
-    let window = WindowBuilder::new()
-        .with_inner_size(LogicalSize::new(SCREEN_WIDTH, SCREEN_HEIGHT))
-        .with_title("Hello framebrush!")
-        .build(&event_loop)
-        .unwrap();
+    let mut buf = vec![0 as u32; DEFAULT_WIDTH * DEFAULT_HEIGHT];
 
-    let context = unsafe { softbuffer::Context::new(&window) }.unwrap();
-    let mut surface = unsafe { softbuffer::Surface::new(&context, &window) }.unwrap();
-    event_loop.run(move |event, _, control_flow| {
-        *control_flow = ControlFlow::Wait;
+    let mut window = Window::new(
+        "Hello, framebrush!",
+        DEFAULT_WIDTH,
+        DEFAULT_HEIGHT,
+        WindowOptions {
+            resize: true,
+            ..Default::default()
+        },
+    )
+    .unwrap();
 
-        match event {
-            Event::MainEventsCleared => {
-                window.request_redraw();
-            }
+    window.set_target_fps(144);
+    while window.is_open() {
+        let (width, height) = window.get_size();
+        buf.resize(width * height, 0);
 
-            Event::RedrawRequested(id) if id == window.id() => {
-                let (width, height) = {
-                    let window_size = window.inner_size();
-
-                    (window_size.width, window_size.height)
-                };
-                if let (Some(width_nonzero), Some(height_nonzero)) =
-                    (NonZeroU32::new(width), NonZeroU32::new(height))
-                {
-                    surface.resize(width_nonzero, height_nonzero).unwrap();
-                    let mut buffer = surface.buffer_mut().unwrap();
-
-                    let mut canvas =
-                        Canvas::new(&mut buffer, (width as usize, height as usize), (320, 240));
-                    canvas.fill(0);
-
-                    canvas.rect(10, 10, 30, 30, &RGBu32::Rgb(190, 96, 105));
-                    buffer.present().expect("Couldn't present frame buffer.");
-                }
-            }
-
-            Event::WindowEvent {
-                window_id: id,
-                event: WindowEvent::CloseRequested,
-            } if id == window.id() => {
-                *control_flow = ControlFlow::Exit;
-            }
-
-            _ => (),
-        }
-    });
+        // Begin drawing
+        let mut canvas = Canvas::new(&mut buf, (width, height), (DEFAULT_WIDTH, DEFAULT_HEIGHT));
+        canvas.fill(0);
+        canvas.rect(10, 10, 30, 30, &RGBu32::Rgb(190, 96, 105));
+        // End drawing
+        window.update_with_buffer(&buf, width, height).unwrap();
+    }
 }
