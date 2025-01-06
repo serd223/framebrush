62476905439c6057ac66adc880b2d6fb64237760
Serdar Çoruhlu <serdar.coruhlu@hotmail.com>
2025-01-06 22:06:59 +0300

    change cube examle to use `minifb` to make it simpler

--- examples/cube.rs
+++ examples/cube.rs
@@ -1,20 +1,76 @@
 use framebrush::{Canvas, RED};
-use std::{
-    num::NonZeroU32,
-    time::{Duration, Instant},
-};
-use winit::{
-    dpi::LogicalSize,
-    event::{Event, WindowEvent},
-    event_loop::{ControlFlow, EventLoop},
-    window::WindowBuilder,
-};
+use minifb::{Window, WindowOptions};
+use std::time::Instant;
 
-const SCREEN_WIDTH: u32 = 640;
-const SCREEN_HEIGHT: u32 = 480;
+const SCREEN_WIDTH: usize = 640;
+const SCREEN_HEIGHT: usize = 480;
 
 const CANVAS_WIDTH: usize = 320;
 const CANVAS_HEIGHT: usize = 240;
+
+fn main() {
+    let cube_vertices = [
+        (-1., -1., -1.),
+        (1., -1., -1.),
+        (-1., -1., 1.),
+        (1., -1., 1.),
+        (-1., 1., -1.),
+        (1., 1., -1.),
+        (-1., 1., 1.),
+        (1., 1., 1.),
+    ];
+    let (mut rot_x, mut rot_y, mut rot_z) = (0., 0., 0.);
+
+    let mut buf = vec![0; SCREEN_WIDTH * SCREEN_HEIGHT];
+    let mut window = Window::new(
+        "Rotating Cube",
+        SCREEN_WIDTH,
+        SCREEN_HEIGHT,
+        WindowOptions {
+            resize: true,
+            ..Default::default()
+        },
+    )
+    .unwrap();
+
+    window.set_target_fps(144);
+    let mut last_frame = Instant::now();
+    while window.is_open() {
+        let delta = {
+            let now = Instant::now();
+            let res = now.duration_since(last_frame).as_secs_f32();
+            last_frame = now;
+            res
+        };
+
+        let (width, height) = window.get_size();
+        buf.resize(width * height, 0);
+
+        rot_x += 1. * delta;
+        rot_y += 5. * delta;
+        rot_z += 3. * delta;
+
+        let cube_transform = cube_vertices.map(|(x, y, z)| {
+            let (x, y, z) = rotate_x(x, y, z, rot_x);
+            let (x, y, z) = rotate_y(x, y, z, rot_y);
+            let (x, y, z) = rotate_z(x, y, z, rot_z);
+
+            let (x, y, z) = ((x + 5.) * 20., (y + 5.) * 20., (z + 5.) * 20.);
+            (x as i32, y as i32, z as i32)
+        });
+
+        let mut canvas = Canvas::new(&mut buf, (width, height), (CANVAS_WIDTH, CANVAS_HEIGHT));
+        canvas.fill(0);
+
+        for (x0, y0, _) in cube_transform {
+            for (x1, y1, _) in cube_transform {
+                canvas.line(x0, y0, x1, y1, &RED);
+            }
+        }
+
+        window.update_with_buffer(&mut buf, width, height).unwrap();
+    }
+}
 
 pub fn rotate_x(x: f32, y: f32, z: f32, rot: f32) -> (f32, f32, f32) {
     (
@@ -36,100 +92,4 @@ pub fn rotate_z(x: f32, y: f32, z: f32, rot: f32) -> (f32, f32, f32) {
         (x * rot.sin()) + (y * rot.cos()),
         z,
     )
-}
-fn main() {
-    let cube_vertices = [
-        (-1., -1., -1.),
-        (1., -1., -1.),
-        (-1., -1., 1.),
-        (1., -1., 1.),
-        (-1., 1., -1.),
-        (1., 1., -1.),
-        (-1., 1., 1.),
-        (1., 1., 1.),
-    ];
-
-    let (mut rot_x, mut rot_y, mut rot_z) = (0., 0., 0.);
-
-    let mut last_redraw = Instant::now();
-    let event_loop = EventLoop::new();
-    let window = WindowBuilder::new()
-        .with_inner_size(LogicalSize::new(SCREEN_WIDTH, SCREEN_HEIGHT))
-        .with_title("Rotating Cube")
-        .build(&event_loop)
-        .unwrap();
-
-    let context = unsafe { softbuffer::Context::new(&window) }.unwrap();
-    let mut surface = unsafe { softbuffer::Surface::new(&context, &window) }.unwrap();
-    event_loop.run(move |event, _, control_flow| {
-        *control_flow = ControlFlow::WaitUntil(
-            Instant::now()
-                .checked_add(Duration::from_micros(1_000_000 / 144))
-                .unwrap(),
-        );
-
-        match event {
-            Event::MainEventsCleared => {
-                let current_time = Instant::now();
-                let frame_time = current_time - last_redraw;
-                let min_frame_time = 17000;
-
-                if frame_time.as_micros() > min_frame_time {
-                    let delta_time = frame_time.as_secs_f32();
-                    rot_x += 1. * delta_time;
-                    rot_y += 5. * delta_time;
-                    rot_z += 3. * delta_time;
-                    last_redraw = current_time;
-                    window.request_redraw();
-                }
-            }
-
-            Event::RedrawRequested(id) if id == window.id() => {
-                let (width, height) = {
-                    let window_size = window.inner_size();
-
-                    (window_size.width, window_size.height)
-                };
-
-                let cube_transform = cube_vertices.map(|(x, y, z)| {
-                    let (x, y, z) = rotate_x(x, y, z, rot_x);
-                    let (x, y, z) = rotate_y(x, y, z, rot_y);
-                    let (x, y, z) = rotate_z(x, y, z, rot_z);
-
-                    let (x, y, z) = ((x + 5.) * 20., (y + 5.) * 20., (z + 5.) * 20.);
-                    (x as i32, y as i32, z as i32)
-                });
-
-                if let (Some(width_nonzero), Some(height_nonzero)) =
-                    (NonZeroU32::new(width), NonZeroU32::new(height))
-                {
-                    surface.resize(width_nonzero, height_nonzero).unwrap();
-                    let mut buffer = surface.buffer_mut().unwrap();
-                    let mut canvas = Canvas::new(
-                        &mut buffer,
-                        (width as usize, height as usize),
-                        (CANVAS_WIDTH, CANVAS_HEIGHT),
-                    );
-                    canvas.fill(0);
-
-                    for (x0, y0, _) in cube_transform {
-                        for (x1, y1, _) in cube_transform {
-                            canvas.line(x0, y0, x1, y1, &RED);
-                        }
-                    }
-
-                    buffer.present().expect("Couldn't present frame buffer.");
-                }
-            }
-
-            Event::WindowEvent {
-                window_id: id,
-                event: WindowEvent::CloseRequested,
-            } if id == window.id() => {
-                *control_flow = ControlFlow::Exit;
-            }
-
-            _ => (),
-        }
-    });
 }
